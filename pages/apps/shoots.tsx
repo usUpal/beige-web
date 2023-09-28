369d57bb220920d91de4b4e8f2321a8661d74f08
Julfiker Ali <60867097+julfiker24878@users.noreply.github.com>
2023-09-28 18:09:51 +0600

    dashboard and shoots page

--- pages/apps/shoots.tsx
+++ pages/apps/shoots.tsx
@@ -7,6 +7,8 @@ import { IRootState } from '../../store';
 import Dropdown from '../../components/Dropdown';
 import { setPageTitle } from '../../store/themeConfigSlice';
 
+import Link from 'next/link';
+
 const tableData = [
     {
         id: 1,
@@ -74,210 +76,123 @@ const Shoots = () => {
         }
     };
     return (
-        <div className="grid grid-cols-1 gap-6 xl:grid-cols-1">
+        <div className="grid grid-cols-1 gap-6 lg:grid-cols-1">
 
-            {/* Simple */}
-            <div className="panel">
+            {/* Recent Orders */}
+            <div className="panel h-full w-full">
                 <div className="mb-5 flex items-center justify-between">
-                    <h5 className="text-lg font-semibold dark:text-white-light">Shoots Table</h5>
-                    <button type="button" onClick={() => toggleCode('code1')} className="font-semibold hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-600">
-                        <span className="flex items-center">
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ltr:mr-2 rtl:ml-2">
-                                <path
-                                    d="M17 7.82959L18.6965 9.35641C20.239 10.7447 21.0103 11.4389 21.0103 12.3296C21.0103 13.2203 20.239 13.9145 18.6965 15.3028L17 16.8296"
-                                    stroke="currentColor"
-                                    strokeWidth="1.5"
-                                    strokeLinecap="round"
-                                />
-                                <path opacity="0.5" d="M13.9868 5L10.0132 19.8297" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-                                <path
-                                    d="M7.00005 7.82959L5.30358 9.35641C3.76102 10.7447 2.98975 11.4389 2.98975 12.3296C2.98975 13.2203 3.76102 13.9145 5.30358 15.3028L7.00005 16.8296"
-                                    stroke="currentColor"
-                                    strokeWidth="1.5"
-                                    strokeLinecap="round"
-                                />
-                            </svg>
-                            Code
-                        </span>
-                    </button>
+                    <h5 className="text-lg font-semibold dark:text-white-light">Recent Orders</h5>
                 </div>
-                <div className="table-responsive mb-5">
+                <div className="table-responsive">
                     <table>
                         <thead>
                             <tr>
-                                <th>Name</th>
-                                <th>Date</th>
-                                <th>Sale</th>
-                                <th>Status</th>
-                                <th className="text-center">Action</th>
+                                <th className="ltr:rounded-l-md rtl:rounded-r-md">Order Name</th>
+                                <th>Order ID</th>
+                                <th>Price</th>
+                                <th>File Status</th>
+                                <th className="ltr:rounded-r-md rtl:rounded-l-md">Status</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {tableData.map((data) => {
-                                return (
-                                    <tr key={data.id}>
-                                        <td>
-                                            <div className="whitespace-nowrap">{data.name}</div>
-                                        </td>
-                                        <td>{data.date}</td>
-                                        <td>{data.sale}</td>
-                                        <td>
-                                            <div
-                                                className={`whitespace-nowrap ${
-                                                    data.status === 'completed'
-                                                        ? 'text-success'
-                                                        : data.status === 'Pending'
-                                                        ? 'text-secondary'
-                                                        : data.status === 'In Progress'
-                                                        ? 'text-info'
-                                                        : data.status === 'Canceled'
-                                                        ? 'text-danger'
-                                                        : 'text-success'
-                                                }`}
-                                            >
-                                                {data.status}
-                                            </div>
-                                        </td>
-                                        <td className="text-center">
-                                            <Tippy content="Delete">
-                                                <button type="button">
-                                                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <path
-                                                            opacity="0.5"
-                                                            d="M9.17065 4C9.58249 2.83481 10.6937 2 11.9999 2C13.3062 2 14.4174 2.83481 14.8292 4"
-                                                            stroke="currentColor"
-                                                            strokeWidth="1.5"
-                                                            strokeLinecap="round"
-                                                        />
-                                                        <path d="M20.5001 6H3.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-                                                        <path
-                                                            d="M18.8334 8.5L18.3735 15.3991C18.1965 18.054 18.108 19.3815 17.243 20.1907C16.378 21 15.0476 21 12.3868 21H11.6134C8.9526 21 7.6222 21 6.75719 20.1907C5.89218 19.3815 5.80368 18.054 5.62669 15.3991L5.16675 8.5"
-                                                            stroke="currentColor"
-                                                            strokeWidth="1.5"
-                                                            strokeLinecap="round"
-                                                        />
-                                                        <path opacity="0.5" d="M9.5 11L10 16" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-                                                        <path opacity="0.5" d="M14.5 11L14 16" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-                                                    </svg>
-                                                </button>
-                                            </Tippy>
-                                        </td>
-                                    </tr>
-                                );
-                            })}
+                            <tr className="group text-white-dark hover:text-black dark:hover:text-white-light/90">
+                                <td className="min-w-[150px] text-black dark:text-white">
+                                    <div className="flex items-center">
+                                        <img className="h-8 w-8 rounded-md object-cover ltr:mr-3 rtl:ml-3" src="/assets/images/ps.svg" alt="avatar" />
+                                        <p className="whitespace-nowrap">
+                                            Video Shoot
+                                            <span className="block text-xs text-[#888EA8]">10 Jan 01:15 PM</span>
+                                        </p>
+                                    </div>
+                                </td>
+                                <td>
+                                    <Link href="/apps/invoice/preview">#46894</Link>
+                                </td>
+                                <td>$56.07</td>
+                                <td className="text-success">Available</td>
+                                <td>
+                                    <span className="badge bg-success shadow-md dark:group-hover:bg-transparent">Completed</span>
+                                </td>
+                            </tr>
+                            <tr className="group text-white-dark hover:text-black dark:hover:text-white-light/90">
+                                <td className="text-black dark:text-white">
+                                    <div className="flex items-center">
+                                        <img className="h-8 w-8 rounded-md object-cover ltr:mr-3 rtl:ml-3" src="/assets/images/cs.svg" alt="avatar" />
+                                        <p className="whitespace-nowrap">
+                                            Commercial Shoot
+                                            <span className="block text-xs text-[#888EA8]">10 Jan 01:15 PM</span>
+                                        </p>
+                                    </div>
+                                </td>
+                                <td>
+                                    <Link href="/apps/invoice/preview">#76894</Link>
+                                </td>
+                                <td>$126.04</td>
+                                <td className="text-success">Available</td>
+                                <td>
+                                    <span className="badge bg-primary shadow-md dark:group-hover:bg-transparent">Post Production</span>
+                                </td>
+                            </tr>
+                            <tr className="group text-white-dark hover:text-black dark:hover:text-white-light/90">
+                                <td className="text-black dark:text-white">
+                                    <div className="flex items-center">
+                                        <img className="h-8 w-8 rounded-md object-cover ltr:mr-3 rtl:ml-3" src="/assets/images/is.svg" alt="avatar" />
+                                        <p className="whitespace-nowrap">
+                                            Photo Shoot
+                                            <span className="block text-xs text-[#888EA8]">10 Jan 01:15 PM</span>
+                                        </p>
+                                    </div>
+                                </td>
+                                <td>
+                                    <Link href="/apps/invoice/preview">#66894</Link>
+                                </td>
+                                <td>$56.07</td>
+                                <td className="text-warning">Not Uploaded</td>
+                                <td>
+                                    <span className="badge bg-dark shadow-md dark:group-hover:bg-transparent">Canceled</span>
+                                </td>
+                            </tr>
+                            <tr className="group text-white-dark hover:text-black dark:hover:text-white-light/90">
+                                <td className="text-black dark:text-white">
+                                    <div className="flex items-center">
+                                        <img className="h-8 w-8 rounded-md object-cover ltr:mr-3 rtl:ml-3" src="/assets/images/ws.svg" alt="avatar" />
+                                        <p className="whitespace-nowrap">
+                                            Wedding Shoot
+                                            <span className="block text-xs text-[#888EA8]">10 Jan 01:15 PM</span>
+                                        </p>
+                                    </div>
+                                </td>
+                                <td>
+                                    <button type="button">#75844</button>
+                                </td>
+                                <td>$110.00</td>
+                                <td className="text-success">Available</td>
+                                <td>
+                                    <span className="badge bg-danger shadow-md dark:group-hover:bg-transparent">In Dispute</span>
+                                </td>
+                            </tr>
+                            <tr className="group text-white-dark hover:text-black dark:hover:text-white-light/90">
+                                <td className="text-black dark:text-white">
+                                    <div className="flex items-center">
+                                        <img className="h-8 w-8 rounded-md object-cover ltr:mr-3 rtl:ml-3" src="/assets/images/cs.svg" alt="avatar" />
+                                        <p className="whitespace-nowrap">
+                                            Commercial Shoot
+                                            <span className="block text-xs text-[#888EA8]">10 Jan 01:15 PM</span>
+                                        </p>
+                                    </div>
+                                </td>
+                                <td>
+                                    <Link href="/apps/invoice/preview">#46894</Link>
+                                </td>
+                                <td>$56.07</td>
+                                <td className="text-warning">Not Uploaded</td>
+                                <td>
+                                    <span className="badge bg-[#C5965C] shadow-md dark:group-hover:bg-transparent">Pending</span>
+                                </td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
-                {codeArr.includes('code1') && (
-                    <CodeHighlight>
-                        <pre className="language-typescript">
-                            {`import Tippy from '@tippyjs/react';
-import 'tippy.js/dist/tippy.css';
-
-const tableData = [
-    {
-        id: 1,
-        name: 'John Doe',
-        email: 'johndoe@yahoo.com',
-        date: '10/08/2020',
-        sale: 120,
-        status: 'Complete',
-        register: '5 min ago',
-        progress: '40%',
-        position: 'Developer',
-        office: 'London',
-    },
-    {
-        id: 2,
-        name: 'Shaun Park',
-        email: 'shaunpark@gmail.com',
-        date: '11/08/2020',
-        sale: 400,
-        status: 'Pending',
-        register: '11 min ago',
-        progress: '23%',
-        position: 'Designer',
-        office: 'New York',
-    },
-    {
-        id: 3,
-        name: 'Alma Clarke',
-        email: 'alma@gmail.com',
-        date: '12/02/2020',
-        sale: 310,
-        status: 'In Progress',
-        register: '1 hour ago',
-        progress: '80%',
-        position: 'Accountant',
-        office: 'Amazon',
-    },
-    {
-        id: 4,
-        name: 'Vincent Carpenter',
-        email: 'vincent@gmail.com',
-        date: '13/08/2020',
-        sale: 100,
-        status: 'Canceled',
-        register: '1 day ago',
-        progress: '60%',
-        position: 'Data Scientist',
-        office: 'Canada',
-    },
-];
-
-<div className="table-responsive mb-5">
-    <table>
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Date</th>
-                <th>Sale</th>
-                <th>Status</th>
-                <th className="text-center">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            {tableData.map((data) => {
-                return (
-                    <tr key={data.id}>
-                        <td>
-                            <div className="whitespace-nowrap">{data.name}</div>
-                        </td>
-                        <td>{data.date}</td>
-                        <td>{data.sale}</td>
-                        <td>
-                            <div
-                                className={\`whitespace-nowrap \${
-                                    data.status === 'completed'
-                                        ? 'text-success'
-                                        : data.status === 'Pending'
-                                        ? 'text-secondary'
-                                        : data.status === 'In Progress'
-                                        ? 'text-info'
-                                        : data.status === 'Canceled'
-                                        ? 'text-danger'
-                                        : 'text-success'
-                                }\`}
-                            >
-                                {data.status}
-                            </div>
-                        </td>
-                        <td className="text-center">
-                            <Tippy content="Delete">
-                                <button type="button">
-                                    <svg>...</svg>
-                                </button>
-                            </Tippy>
-                        </td>
-                    </tr>
-                );
-            })}
-        </tbody>
-    </table>
-</div>`}
-                        </pre>
-                    </CodeHighlight>
-                )}
             </div>
 
         </div>
