d4e396b7e6cf006ebe42b70131efab672c9ad5c5
wEbCoAdEr <blackmanin062@gmail.com>
2023-10-09 14:24:42 +0600

    Removed BaseURL tsx

--- baseURL.tsx
+++
@@ -1,4 +0,0 @@
-// export const baseURL = 'http://localhost:5000/v1';
-// export const rootURL = 'http://localhost:5000';
-export const baseURL = 'https://api.beigecorporation.io/v1';
-// export const rootURL = 'https://api.beigecorporation.io';
