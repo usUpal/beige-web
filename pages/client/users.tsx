19cdc33553bd1069f0821b8619db7820d613d48c
Julfiker Ali <60867097+julfiker24878@users.noreply.github.com>
2023-11-08 18:06:58 +0600

    minor modifications

--- pages/client/users.tsx
+++ pages/client/users.tsx
@@ -24,9 +24,9 @@ const Users = () => {
   const getAllUsers = async () => {
     try {
       const response = await fetch(`https://api.beigecorporation.io/v1/cp`);
-      const allShots = await response.json();
-      setTotalPagesCount(allShots?.totalPages);
-      setAllUsers(allShots.results);
+      const users = await response.json();
+      setTotalPagesCount(users?.totalPages);
+      setAllUsers(users.results);
       console.log(allUsers);
     } catch (error) {
       console.error(error);
@@ -34,20 +34,20 @@ const Users = () => {
   };
 
   // User Single
-  const getShootDetails = async (shootId: string) => {
+  const getUserDetails = async (singleUserId: string) => {
     setLoading(true);
     try {
-      const response = await fetch(`https://api.beigecorporation.io/v1/cp/${shootId}`);
-      const shootDetailsRes = await response.json();
+      const response = await fetch(`https://api.beigecorporation.io/v1/cp/${singleUserId}`);
+      const userDetailsRes = await response.json();
 
-      if (!shootDetailsRes) {
-        console.log(response);
+      if (!userDetailsRes) {
         setShowError(true);
         setLoading(false);
       } else {
-        setShootInfo(shootDetailsRes);
+        setShootInfo(userDetailsRes);
         setLoading(false);
         setshootModal(true);
+        console.log(shootInfo);
       }
     } catch (error) {
       console.error(error);
@@ -75,11 +75,13 @@ const Users = () => {
 
   const handleChange = (e: any) => {
     const { name, value, type } = e.target;
-
-    // Handle different input types (text, number, select, checkbox) appropriately
     const newValue = type === 'checkbox' ? e.target.checked : value;
-
-    // Update the 'isData' state with the new value
+    console.log(
+      {
+        "VALUE": value,
+        "NEW VALUE": newValue
+      }
+    );
   }
 
   return (
@@ -118,30 +120,30 @@ const Users = () => {
                       </thead>
                       <tbody>
 
-                      {allUsers?.map((user) => (
-                        <tr key={user.id} className="group text-white-dark hover:text-black dark:hover:text-white-light/90">
-                          <td className="min-w-[150px] text-black dark:text-white font-sans">
-                            <div className="flex items-center">
-                              <p className="whitespace-nowrap">{user?.userId?.id}</p>
-                            </div>
-                          </td>
-                          <td>{user?.userId?.name}</td>
-                          <td>{user?.userId?.email}</td>
-                          <td className="text-success font-sans">{user?.userId?.role}</td>
-                          <td>
-                            <div className="font-sans">
-                              Active
-                            </div>
-                          </td>
-                          <td>
-                            <button type="button" className="p-0" onClick={() => getShootDetails(user.userId.id)}>
-                              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M11.4001 18.1612L11.4001 18.1612L18.796 10.7653C17.7894 10.3464 16.5972 9.6582 15.4697 8.53068C14.342 7.40298 13.6537 6.21058 13.2348 5.2039L5.83882 12.5999L5.83879 12.5999C5.26166 13.1771 4.97307 13.4657 4.7249 13.7838C4.43213 14.1592 4.18114 14.5653 3.97634 14.995C3.80273 15.3593 3.67368 15.7465 3.41556 16.5208L2.05445 20.6042C1.92743 20.9852 2.0266 21.4053 2.31063 21.6894C2.59466 21.9734 3.01478 22.0726 3.39584 21.9456L7.47918 20.5844C8.25351 20.3263 8.6407 20.1973 9.00498 20.0237C9.43469 19.8189 9.84082 19.5679 10.2162 19.2751C10.5343 19.0269 10.823 18.7383 11.4001 18.1612Z" fill="currentColor"></path>
-                                <path d="M20.8482 8.71306C22.3839 7.17735 22.3839 4.68748 20.8482 3.15178C19.3125 1.61607 16.8226 1.61607 15.2869 3.15178L14.3999 4.03882C14.4121 4.0755 14.4246 4.11268 14.4377 4.15035C14.7628 5.0875 15.3763 6.31601 16.5303 7.47002C17.6843 8.62403 18.9128 9.23749 19.85 9.56262C19.8875 9.57563 19.9245 9.58817 19.961 9.60026L20.8482 8.71306Z" fill="currentColor"></path>
-                              </svg>
-                            </button>
-                          </td>
-                        </tr>
+                        {allUsers?.map((user) => (
+                          <tr key={user.id} className="group text-white-dark hover:text-black dark:hover:text-white-light/90">
+                            <td className="min-w-[150px] text-black dark:text-white font-sans">
+                              <div className="flex items-center">
+                                <p className="whitespace-nowrap">{user?.userId?.id}</p>
+                              </div>
+                            </td>
+                            <td>{user?.userId?.name}</td>
+                            <td>{user?.userId?.email}</td>
+                            <td className="text-success font-sans">{user?.userId?.role}</td>
+                            <td>
+                              <div className="font-sans">
+                              {user?.userId?.isEmailVerified === true ? "Verified" : "Unverified"}
+                              </div>
+                            </td>
+                            <td>
+                              <button type="button" className="p-0" onClick={() => getUserDetails(user.userId.id)}>
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                  <path d="M11.4001 18.1612L11.4001 18.1612L18.796 10.7653C17.7894 10.3464 16.5972 9.6582 15.4697 8.53068C14.342 7.40298 13.6537 6.21058 13.2348 5.2039L5.83882 12.5999L5.83879 12.5999C5.26166 13.1771 4.97307 13.4657 4.7249 13.7838C4.43213 14.1592 4.18114 14.5653 3.97634 14.995C3.80273 15.3593 3.67368 15.7465 3.41556 16.5208L2.05445 20.6042C1.92743 20.9852 2.0266 21.4053 2.31063 21.6894C2.59466 21.9734 3.01478 22.0726 3.39584 21.9456L7.47918 20.5844C8.25351 20.3263 8.6407 20.1973 9.00498 20.0237C9.43469 19.8189 9.84082 19.5679 10.2162 19.2751C10.5343 19.0269 10.823 18.7383 11.4001 18.1612Z" fill="currentColor"></path>
+                                  <path d="M20.8482 8.71306C22.3839 7.17735 22.3839 4.68748 20.8482 3.15178C19.3125 1.61607 16.8226 1.61607 15.2869 3.15178L14.3999 4.03882C14.4121 4.0755 14.4246 4.11268 14.4377 4.15035C14.7628 5.0875 15.3763 6.31601 16.5303 7.47002C17.6843 8.62403 18.9128 9.23749 19.85 9.56262C19.8875 9.57563 19.9245 9.58817 19.961 9.60026L20.8482 8.71306Z" fill="currentColor"></path>
+                                </svg>
+                              </button>
+                            </td>
+                          </tr>
                         ))}
 
                       </tbody>
@@ -214,7 +216,7 @@ const Users = () => {
                                     <label htmlFor="trust_score" className="text-[14px] mb-0 rtl:ml-2 sm:w-1/4 sm:ltr:mr-2 font-sans capitalize">
                                       average rating
                                     </label>
-                                    <input id="average_rating" type="number" defaultValue={shootInfo?.average_rating}className="form-input" name="average_rating" onChange={handleChange} />
+                                    <input id="average_rating" type="number" defaultValue={shootInfo?.average_rating} className="form-input" name="average_rating" onChange={handleChange} />
                                   </div>
                                 </div>
                                 <div className="flex items-center justify-between">
@@ -344,12 +346,12 @@ const Users = () => {
                                     </label>
                                     <input id="num_no_shows" type="number" defaultValue={shootInfo?.num_no_shows} className="form-input block font-sans" name='num_no_shows' onChange={handleChange} />
                                   </div>
-                                  {/* User ID */}
+                                  {/* Email Verified */}
                                   <div className="flex basis-[45%] flex-col sm:flex-row">
-                                    <label htmlFor="userId" className="text-[14px] mb-0 rtl:ml-2 sm:w-1/4 sm:ltr:mr-2 font-sans capitalize">
-                                      user ID
+                                    <label htmlFor="isEmailVerified" className="text-[14px] mb-0 rtl:ml-2 sm:w-1/4 sm:ltr:mr-2 font-sans capitalize">
+                                    Email Verified
                                     </label>
-                                    <input id="userId" type="text" defaultValue={shootInfo?.id} className="form-input block font-sans" name='userId' onChange={handleChange} />
+                                    <input id="isEmailVerified" type="text" defaultValue={shootInfo?.userId.role} className="form-input block font-sans" name='isEmailVerified' onChange={handleChange} />
                                   </div>
                                 </div>
                                 <div className="flex items-center justify-between">
