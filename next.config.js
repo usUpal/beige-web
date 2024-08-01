a81e46db8c3f3c45abfeb5b02efb2a3fb4c166bc
shakib <shakibmiah554@gmail.com>
2024-08-01 18:56:37 +0600

    env config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        ignoreBuildErrors: true,
-      },
+  reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  env: {
+    API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
+    SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_API_ENDPOINT,
+    googleClientId: process.env.googleClientId,
+    CDN_URL: process.env.CDN_URL,
+    BucketUrl: process.env.BucketUrl,
+    appName: process.env.appName,
+  },
 };
 
 module.exports = nextConfig;
