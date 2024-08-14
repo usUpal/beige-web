daefa9203ff68f3b473406e9f5472052c02ecfe0
noman-swe <noman.swe130@gmail.com>
2024-08-14 11:32:01 +0600

    [updated payouts with status-update]

--- pages/dashboard/transactions.tsx
+++ pages/dashboard/transactions.tsx
@@ -1,67 +1,11 @@
 import { Fragment, useEffect, useRef, useState } from 'react';
-import CodeHighlight from '../../components/Highlight';
-import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { IRootState } from '../../store';
-import Dropdown from '../../components/Dropdown';
 import { setPageTitle } from '../../store/themeConfigSlice';
 import { API_ENDPOINT } from '@/config';
-import { useAuth } from '@/contexts/authContext';
 import { allSvgs } from '@/utils/allsvgs/allSvgs';
 import { Dialog, Transition } from '@headlessui/react';
-import StatusBg from '@/components/Status/StatusBg';
-
-const tableData = [
-  {
-    id: 1,
-    name: 'John Doe',
-    email: 'johndoe@yahoo.com',
-    date: '10/08/2020',
-    sale: 120,
-    status: 'Complete',
-    register: '5 min ago',
-    progress: '40%',
-    position: 'Developer',
-    office: 'London',
-  },
-  {
-    id: 2,
-    name: 'Shaun Park',
-    email: 'shaunpark@gmail.com',
-    date: '11/08/2020',
-    sale: 400,
-    status: 'Pending',
-    register: '11 min ago',
-    progress: '23%',
-    position: 'Designer',
-    office: 'New York',
-  },
-  {
-    id: 3,
-    name: 'Alma Clarke',
-    email: 'alma@gmail.com',
-    date: '12/02/2020',
-    sale: 310,
-    status: 'In Progress',
-    register: '1 hour ago',
-    progress: '80%',
-    position: 'Accountant',
-    office: 'Amazon',
-  },
-  {
-    id: 4,
-    name: 'Vincent Carpenter',
-    email: 'vincent@gmail.com',
-    date: '13/08/2020',
-    sale: 100,
-    status: 'Canceled',
-    register: '1 day ago',
-    progress: '60%',
-    position: 'Data Scientist',
-    office: 'Canada',
-  },
-];
 
 const Transactions = () => {
   const dispatch = useDispatch();
