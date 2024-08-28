ac201107a62ab1e06d5ae5df30e0921b26d6f0ed
shohagrana006 <shohagrana006@gmail.com>
2024-08-28 17:28:10 +0600

    chat list three dots

--- pages/dashboard/chat.tsx
+++ pages/dashboard/chat.tsx
@@ -45,6 +45,11 @@ const Chat = () => {
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [totalPagesCount, setTotalPagesCount] = useState<number>(1);
 
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
   };
@@ -197,6 +202,18 @@ const Chat = () => {
   const getChatByQuery = (event) => {
     setSearchUser(event.target.value);
   }
+
+  const users = [
+    { name: 'John Doe', isActive: true },
+    { name: 'Jane Smith', isActive: false },
+    { name: 'Alice Johnson', isActive: true },
+    { name: 'Bob Brown', isActive: false },
+    { name: 'Charlie Green', isActive: true }
+  ];
+
+  const totalUsers = users.length;
+  const totalActiveUsers = users.filter(user => user.isActive).length;
+  const totalInactiveUsers = totalUsers - totalActiveUsers;
 
   return (
     <div>
@@ -426,25 +443,69 @@ const Chat = () => {
                     </Link>
                   </div>
                 </div>
-                {/* <div className="flex gap-3 sm:gap-5">
+                <div className="flex gap-3 sm:gap-5">
                   <div className="dropdown">
                     <Dropdown
                       placement={`${isRtl ? 'bottom-start' : 'bottom-end'}`}
                       btnClassName="bg-[#f4f4f4] dark:bg-[#1b2e4b] hover:bg-primary-light w-8 h-8 rounded-full !flex justify-center items-center"
                       button={
-                        <svg className="shrink-0 ltr:mr-2 rtl:ml-2" width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                          <circle cx="12" cy="12" r="3" stroke="currentColor" strokeWidth="1.5"></circle>
-                          <path
-                            opacity="0.5"
-                            d="M13.7654 2.15224C13.3978 2 12.9319 2 12 2C11.0681 2 10.6022 2 10.2346 2.15224C9.74457 2.35523 9.35522 2.74458 9.15223 3.23463C9.05957 3.45834 9.0233 3.7185 9.00911 4.09799C8.98826 4.65568 8.70226 5.17189 8.21894 5.45093C7.73564 5.72996 7.14559 5.71954 6.65219 5.45876C6.31645 5.2813 6.07301 5.18262 5.83294 5.15102C5.30704 5.08178 4.77518 5.22429 4.35436 5.5472C4.03874 5.78938 3.80577 6.1929 3.33983 6.99993C2.87389 7.80697 2.64092 8.21048 2.58899 8.60491C2.51976 9.1308 2.66227 9.66266 2.98518 10.0835C3.13256 10.2756 3.3397 10.437 3.66119 10.639C4.1338 10.936 4.43789 11.4419 4.43786 12C4.43783 12.5581 4.13375 13.0639 3.66118 13.3608C3.33965 13.5629 3.13248 13.7244 2.98508 13.9165C2.66217 14.3373 2.51966 14.8691 2.5889 15.395C2.64082 15.7894 2.87379 16.193 3.33973 17C3.80568 17.807 4.03865 18.2106 4.35426 18.4527C4.77508 18.7756 5.30694 18.9181 5.83284 18.8489C6.07289 18.8173 6.31632 18.7186 6.65204 18.5412C7.14547 18.2804 7.73556 18.27 8.2189 18.549C8.70224 18.8281 8.98826 19.3443 9.00911 19.9021C9.02331 20.2815 9.05957 20.5417 9.15223 20.7654C9.35522 21.2554 9.74457 21.6448 10.2346 21.8478C10.6022 22 11.0681 22 12 22C12.9319 22 13.3978 22 13.7654 21.8478C14.2554 21.6448 14.6448 21.2554 14.8477 20.7654C14.9404 20.5417 14.9767 20.2815 14.9909 19.902C15.0117 19.3443 15.2977 18.8281 15.781 18.549C16.2643 18.2699 16.8544 18.2804 17.3479 18.5412C17.6836 18.7186 17.927 18.8172 18.167 18.8488C18.6929 18.9181 19.2248 18.7756 19.6456 18.4527C19.9612 18.2105 20.1942 17.807 20.6601 16.9999C21.1261 16.1929 21.3591 15.7894 21.411 15.395C21.4802 14.8691 21.3377 14.3372 21.0148 13.9164C20.8674 13.7243 20.6602 13.5628 20.3387 13.3608C19.8662 13.0639 19.5621 12.558 19.5621 11.9999C19.5621 11.4418 19.8662 10.9361 20.3387 10.6392C20.6603 10.4371 20.8675 10.2757 21.0149 10.0835C21.3378 9.66273 21.4803 9.13087 21.4111 8.60497C21.3592 8.21055 21.1262 7.80703 20.6602 7C20.1943 6.19297 19.9613 5.78945 19.6457 5.54727C19.2249 5.22436 18.693 5.08185 18.1671 5.15109C17.9271 5.18269 17.6837 5.28136 17.3479 5.4588C16.8545 5.71959 16.2644 5.73002 15.7811 5.45096C15.2977 5.17191 15.0117 4.65566 14.9909 4.09794C14.9767 3.71848 14.9404 3.45833 14.8477 3.23463C14.6448 2.74458 14.2554 2.35523 13.7654 2.15224Z"
-                            stroke="currentColor"
-                            strokeWidth="1.5"
-                          ></path>
+                        <svg
+                          viewBox="0 0 22 22"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="#000000"
+                          className="bi bi-three-dots-vertical ml-2 mt-1"
+                          onClick={toggleSidebar}
+                        >
+                          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                          <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                          <g id="SVGRepo_iconCarrier">
+                            <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"></path>
+                          </g>
                         </svg>
                       }
                     ></Dropdown>
                   </div>
-                </div> */}
+
+
+                  {/* Sidebar Modal */}
+                  {isSidebarOpen && (
+                    <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-end">
+                      <div className="bg-white dark:bg-[#1b2e4b] w-64 h-full shadow-lg p-4">
+                        <button
+                          className="text-black dark:text-white"
+                          onClick={toggleSidebar}
+                        >
+                         <div class="style_editorClose__7uxp1"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 15" fill="#555" width="26" height="26"><path fill-rule="evenodd" clip-rule="evenodd" d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z" fill="currentColor"></path></svg></div>
+                        </button>
+                       
+                        <div>
+
+                          <div className="flex justify-between items-center bg-gray-100 dark:bg-[#2c3e50] p-2 rounded-lg mb-4">
+                            <span className="text-black dark:text-white text-sm font-medium">All: ({totalUsers})</span>
+                            <span className="text-blue-500 dark:text-blue-400 text-sm font-medium">Active: ({totalActiveUsers})</span>
+                            <span className="text-gray-500 dark:text-gray-400 text-sm font-medium">Inactive: ({totalInactiveUsers})</span>
+                          </div>
+
+                          <ul className="space-y-2">
+                            {users.map((user, index) => (
+                              <li key={index} className="flex items-center hover:bg-gray-200 dark:hover:bg-[#2c3e50] p-2 rounded">
+                                <span
+                                  className={`rounded-full w-2.5 h-2.5 mr-2 ${
+                                    user.isActive ? 'bg-blue-500' : 'bg-gray-400'
+                                  }`}
+                                ></span>
+                                <span className="text-black dark:text-white">{user.name}</span>
+                              </li>
+                            ))}
+                          </ul>
+                          
+                        </div>
+                      </div>
+                    </div>
+                  )}
+
+
+                </div> 
               </div>
               <div className="h-px w-full border-b border-white-light dark:border-[#1b2e4b]"></div>
 
